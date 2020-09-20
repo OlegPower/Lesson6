088161da2c49b7aa18471f280dd55f3dd7cc9dfe
OlegPower <petkoo@ukr.net>
2020-09-20 10:36:28 +0300

    TEST21

--- src/main/main1.java
+++ src/main/main1.java
@@ -2,7 +2,8 @@ package main;
 
 public class main1 {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        System.out.println("Hi 21");
         System.out.println("Chao");
+        System.out.println("Chao-cacao");
     }
 }
